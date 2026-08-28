df00c39886a1bd7829b7f1e3cf2decb9f5da37cc
Jonathan Heredia <shakatoti1618@gmail.com>
2026-08-28 15:46:29 -0500

    feat(types): agrega barrel de exportacion de tipos

---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export * from "./api";
+export * from "./auth";
+export * from "./balance";
+export * from "./currency";
+export * from "./transaction";
+export * from "./user";
+export * from "./wallet";
