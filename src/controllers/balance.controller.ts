caf9cc981e06105c704cbc43b9410e26dcb28ef0
Paul Pardini <paulpardini.it@gmail.com>
2026-08-30 17:33:47 +0200

    Conecto a DB test ok conexion realizada sprint 1

--- src/controllers/balance.controller.ts
+++ src/controllers/balance.controller.ts
@@ -1,10 +1,15 @@
 import type { Request, Response } from "express";
+import { getWalletByUserId } from "../services/wallet.service";
+import { getBalancesByWalletId } from "../services/balance.service";
 
 export const getBalances = async (
   _req: Request,
   res: Response
 ): Promise<void> => {
-  res.status(501).json({
-    message: "Balances pendientes de conexión con servicio",
-  });
+  const userId = res.locals.userId;
+
+  const wallet = await getWalletByUserId(userId);
+  const balances = await getBalancesByWalletId(wallet.id);
+
+  res.status(200).json({ balances });
 };
